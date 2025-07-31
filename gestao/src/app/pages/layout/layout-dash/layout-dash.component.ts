e28a801083cbc5f6abb89966b367066a4e46de76
Yuri Fernandes <devyuri.programador@gmail.com>
2025-07-31 06:47:42 -0300

    Componente barra-principal

--- gestao/src/app/pages/layout/layout-dash/layout-dash.component.ts
+++ gestao/src/app/pages/layout/layout-dash/layout-dash.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BarraPrincipalComponent } from "../../../components/barra-principal/barra-principal.component";
 
 @Component({
   selector: 'app-layout-dash',
   standalone: true,
-  imports: [],
+  imports: [BarraPrincipalComponent],
   templateUrl: './layout-dash.component.html',
   styleUrl: './layout-dash.component.css'
 })
