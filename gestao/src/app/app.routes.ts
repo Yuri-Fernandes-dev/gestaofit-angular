071b0fdca56169c21850c4237a4a2ad56dee5710
Yuri Fernandes <devyuri.programador@gmail.com>
2025-07-31 05:10:32 -0300

    Pagina de cadastro

--- gestao/src/app/app.routes.ts
+++ gestao/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LayoutCadastroComponent } from './pages/layout/layout-cadastro/layout-cadastro.component';
+import { LayoutLoginComponent } from './pages/layout/layout-login/layout-login.component';
 
 export const routes: Routes = [
+    {path: '', component: LayoutLoginComponent},
+    {path: 'cadastro', component:LayoutCadastroComponent}
     
 ];
--- gestao/src/app/pages/layout/layout-cadastro/layout-cadastro.component.ts
+++ gestao/src/app/pages/layout/layout-cadastro/layout-cadastro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderLoginComponent } from "../../../components/header-login/header-login.component";
 
 @Component({
   selector: 'app-layout-cadastro',
   standalone: true,
-  imports: [],
+  imports: [HeaderLoginComponent],
   templateUrl: './layout-cadastro.component.html',
   styleUrl: './layout-cadastro.component.css'
 })
