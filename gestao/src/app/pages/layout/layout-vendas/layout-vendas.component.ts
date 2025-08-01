c2f75f592bbe3cd5b32e178d2875c71838f47b33
Yuri Fernandes <devyuri.programador@gmail.com>
2025-08-01 12:51:15 -0300

    Pagina de Vendas PDV

--- gestao/src/app/pages/layout/layout-vendas/layout-vendas.component.ts
+++ gestao/src/app/pages/layout/layout-vendas/layout-vendas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BarraPrincipalComponent } from "../../../components/barra-principal/barra-principal.component";
+import { BtnBasicoComponent } from "../../../components/btn-basico/btn-basico.component";
 
 @Component({
   selector: 'app-layout-vendas',
   standalone: true,
-  imports: [BarraPrincipalComponent],
+  imports: [BarraPrincipalComponent, BtnBasicoComponent],
   templateUrl: './layout-vendas.component.html',
   styleUrl: './layout-vendas.component.css'
 })
